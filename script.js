dc948dbe2be21e29b337934ce2e4bbd2f235d3a5
lendrit21 <72894376+lendrit21@users.noreply.github.com>
2023-01-04 10:13:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+// login form (become a customer)
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = () =>{
+  document.querySelector('.login-form-container').classList.toggle('active');
+}
+
+document.querySelector('#close-login-form').onclick = () =>{
+  document.querySelector('.login-form-container').classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('header').classList.add('active');
+  }else{
+    document.querySelector('header').classList.remove('active');
+  };
+};
+
+// end of login form
+
+
 // rregullimi i register dhe login ne login form
 var x =document.getElementById("login");
 var y =document.getElementById("register");
